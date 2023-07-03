83abeb67c0d71defa1328278d9529bdd0ebee0d6
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-07-03 23:09:43 +0530

    Create ques-0268.java

---
+++ Leetcode-problems/ques-0268.java
@@ -0,0 +1,15 @@
+// 268. MISSING NUMBER
+
+class Solution {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        int n = nums.length;
+
+        for(int i=0; i<n; i++){
+           if(i != nums[i])
+           return i;
+        }
+        return n;
+    }
+}
