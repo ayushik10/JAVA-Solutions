8ef8a04b0f1a819233660d121edf4c75f4b289f4
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-07-04 21:32:07 +0530

    Create 0169-Missing-Number.java

---
+++ Leetcode-problems/0169-Missing-Number.java
@@ -0,0 +1,10 @@
+// 169. MISSING NUMBER
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int n = nums.length;
+        
+        Arrays.sort(nums);
+        return nums[n/2];
+    }
+}
