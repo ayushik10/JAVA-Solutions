da49b423ff95f4dc1501d0d9ab394cfd0ffc7122
Ayushi Kumari <115365818+ayushik10@users.noreply.github.com>
2023-07-26 22:26:39 +0530

    Create 2710-Remove-Trailing-Zero-From-A-String.java

---
+++ Leetcode-problems/2710-Remove-Trailing-Zero-From-A-String.java
@@ -0,0 +1,16 @@
+//REMOVE TRAILING ZEROES FROM A STRING
+
+class Solution {
+    public String removeTrailingZeros(String num) {
+        
+        int n = num.length()-1;
+
+        for(int i=n; i>0; i--) {
+            if(num.charAt(i) != '0') {
+                break;
+            }
+            n--;
+        }
+        return num.substring(0, n+1);
+    }
+}
